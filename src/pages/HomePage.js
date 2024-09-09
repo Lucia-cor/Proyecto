0809b2a695fadfc32b5786acd4be4cbb9d4e428c
Lucia Cordone <matactivo.ep@gmail.com>
2024-09-09 20:26:24 +0200

    comienzo armado home

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,6 +6,12 @@ const HomePage = (props) => {
             <div className="imagen">
                 <img src="IMG/Home/Vista primavera.jpg" width="100%" alt="primavera" />
             </div>
+            <div className="columnas">
+                <section className="introduccion">
+                    <h2>Descansa en la natraleza</h2>
+                </section>
+
+            </div>
 
         </main>
 
