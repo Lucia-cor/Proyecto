070960b4d2f7ad14a9da6ac9180a0bc861333021
Lucia Cordone <matactivo.ep@gmail.com>
2024-09-11 18:25:41 +0200

    continuacion proyecto

--- src/componentes/Header.js
+++ src/componentes/Header.js
@@ -3,7 +3,7 @@ const Header = (props) => {
     return (
         <header>
             <div>
-                <img src="public\Logo.png" width="200" alt="Logo"></img>
+                <img src="IMG\Logo.png" width="200" alt="Logo"></img>
                 <h1>Cabaña Cordón Situación</h1>
             </div>
         </header>
--- src/pages/NosotrosPage.js
+++ src/pages/NosotrosPage.js
@@ -1,8 +1,24 @@
 import React from "react";
 
 const NosotrosPage = (props) => {
-    return(
-        <div>NosotrosPage</div>
+    return (
+        <main className="holder">
+            <div className="imagen">
+                <img src="IMG/Home/Cabaña roja.jpg" alt="cabaña roja" />
+            </div>
+            <div className="descripcion">
+                <h2>Quienes somos...</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum nesciunt, expedita quas ex velit quisquam
+                    officia cupiditate aliquam ducimus consequatur eveniet provident. Doloremque rerum, sed assumenda quia
+                    nemo
+                    aspernatur vel. Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt, magnam! Totam, porro
+                    assumenda non expedita dolore aliquam officiis, tenetur tempora neque natus enim animi optio beatae
+                    maxime earum quis. Rerum!</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, corrupti illum consequuntur, doloremque
+                    suscipit magnam tenetur eum porro facere, sed dicta delectus et ipsa molestiae? Exercitationem
+                    doloremque libero amet aspernatur?</p>
+            </div>
+        </main>
     );
 }
 
